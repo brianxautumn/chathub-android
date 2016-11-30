74ac33116129a78f250c3ef0ccbb2f6a289ded7e
brianxautumn <shred@brianautumn.com>
2016-11-30 14:16:55 -0800

    Cleaned up messaging service

--- app/src/main/java/edu/sfsu/csc780/chathub/service/PrivateMessagingService.java
+++ app/src/main/java/edu/sfsu/csc780/chathub/service/PrivateMessagingService.java
@@ -1,7 +1,7 @@
 package edu.sfsu.csc780.chathub.service;
 
 import android.app.IntentService;
-import android.app.NotificationManager;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
@@ -9,9 +9,11 @@ import android.content.Context;
 import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Debug;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
@@ -35,14 +37,17 @@ import edu.sfsu.csc780.chathub.ui.InboxUtil;
 
 
 public class PrivateMessagingService extends Service {
+    final static String GROUP_KEY = "group_key_chathub";
 
     // Firebase instance variables
+
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
     private DatabaseReference mFirebaseDatabaseReference;
     private DatabaseReference mActiveThreads;
-    private long startTime;
-
+    private long latestTimestamp;
+    private int unreadNotificationCount = 0;
+    private int notificationCount = 0;
 
     @Override
     public int onStartCommand (Intent intent,  int flags, int startId){
@@ -50,7 +55,7 @@ public class PrivateMessagingService extends Service {
         //Initialize Auth
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
-        startTime = new Date().getTime();
+        latestTimestamp = new Date().getTime();
 
 
         //Now set up a listener for the user's contacts
@@ -64,15 +69,27 @@ public class PrivateMessagingService extends Service {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    PrivateThread threadData = postSnapshot.getValue(PrivateThread.class);
+                    Log.d("SERVICE", threadData.getMessage());
+                    if(threadData.getTimestamp() > latestTimestamp){
+                        latestTimestamp = new Date().getTime();
 
+                        createNotification(threadData.getMessage(), threadData.getName());
+
+                    }
+                    /*
                      DatabaseReference chatRef = postSnapshot.getRef();
                         chatRef.addValueEventListener(new ValueEventListener() {
                         @Override
                         public void onDataChange(DataSnapshot dataSnapshot) {
                             PrivateThread threadData = dataSnapshot.getValue(PrivateThread.class);
-                            if(threadData.getTimestamp() > startTime){
+                            if(threadData.getTimestamp() > latestTimestamp){
+
+                                Log.d("SERVICE", "stuff");
                                 createNotification(threadData.getMessage(), threadData.getName());
+
                             }
+                            latestTimestamp = new Date().getTime();
                         }
 
                         @Override
@@ -80,8 +97,10 @@ public class PrivateMessagingService extends Service {
 
                         }
                     });
+                    */
 
                 }
+
                 //createNotification(post.getMessage(), "test");
             }
 
@@ -110,6 +129,8 @@ public class PrivateMessagingService extends Service {
                         .setContentText(message)
                         .setLights(Color.BLUE, 500, 500)
                         .setStyle(new NotificationCompat.InboxStyle())
+                        .setGroup(GROUP_KEY)
+                        .setGroupSummary(true)
                         .setSound(alarmSound);
 
         // Creates an explicit intent for an Activity in your app
@@ -125,9 +146,10 @@ public class PrivateMessagingService extends Service {
                         PendingIntent.FLAG_UPDATE_CURRENT
                 );
         mBuilder.setContentIntent(resultPendingIntent);
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        // mId allows you to update the notification later on.
-        mNotificationManager.notify(0, mBuilder.build());
+        NotificationManagerCompat mNotificationManager = NotificationManagerCompat.from(this);
+
+        mNotificationManager.notify(notificationCount++, mBuilder.build());
+
+
     }
 }
